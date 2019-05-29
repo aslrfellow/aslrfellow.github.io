8f31928b8f35f187cc50e5db46ce2c6561a4985f
aslrfellow <50293137+aslrfellow@users.noreply.github.com>
2019-05-28 22:33:50 -0500

    Create helloworld32.asm

---
+++ code/lesson1/helloworld32.asm
@@ -0,0 +1,17 @@
+; nasm -f macho helloworld32.asm && ld -macosx_version_min 10.7.0 -o helloworld32 helloworld32.o && ./helloworld32
+
+global start
+
+section .text
+start:
+    push    dword msg.len
+    push    dword msg
+    push    dword 1
+    mov     eax, 4
+    sub     esp, 4
+    int     0x80
+
+section .data
+
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
